af8e1b8c08fe187bf97b777a75eed6fe1335a695
neverdieone <altiore@mail.ru>
2022-06-19 08:44:36 +0000

    Add prepare reviwes

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -11,6 +11,33 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func getReviews(c *http.Client, t string, u string) (map[string]interface{}, error) {
+	req, err := http.NewRequest("GET", u, nil)
+	req.Header.Set("Authorization", "Token "+t)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	response, err := c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reviews := map[string]interface{}{}
+	if err := json.Unmarshal(body, &reviews); err != nil {
+		log.Fatal(err)
+	}
+
+	return reviews, nil
+}
+
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal(err)
@@ -19,27 +46,27 @@ func main() {
 	token := os.Getenv("DVMN_TOKEN")
 	c := &http.Client{Timeout: 95 * time.Second}
 	url := "https://dvmn.org/api/long_polling/"
+	var timestamp interface{}
 
 	for {
-		req, err := http.NewRequest("GET", url, nil)
-		req.Header.Set("Authorization", "Token "+token)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		response, err := c.Do(req)
+		reviews, err := getReviews(c, token, url)
 		if err != nil {
 			log.Println(err)
 			continue
 		}
 
-		defer response.Body.Close()
-
-		body, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			log.Fatal(err)
+		status := reviews["status"]
+		switch status {
+		case "timeut":
+			timestamp = reviews["timestamp_to_request"]
+		case "found":
+			timestamp = reviews["last_attempt_timestamp"]
+			attempts, _ := reviews["new_attempts"].([]interface{})
+			log.Println(attempts)
+		default:
+			log.Println("Unexpected status: " + status.(string))
 		}
 
-		fmt.Println(string(body))
+		log.Println(timestamp)
 	}
 }
